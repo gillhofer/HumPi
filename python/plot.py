5a0b6222a0d84576456496aea698a0b18de3183a
Gillhofer Michael <michael.gillhofer@fill.co.at>
2020-10-27 21:39:19 +0100

    adapt plot to different data_format

--- python/plot.py
+++ python/plot.py
@@ -17,7 +17,7 @@ args = parser.parse_args()
 uri = "mongodb://%s:%s@%s" % (
     quote_plus(args.serveruser), quote_plus(args.serverpassword), quote_plus(args.serverurl))
 client = MongoClient(uri)
-db = client.gridfrequency.raw
+db = client.gridfrequency.measuerment
 
 stop = datetime.fromisoformat("2020-10-21T00:23:50.676+00:00").replace(tzinfo=None)
 start = datetime.fromisoformat("2020-10-21T00:23:37.856+00:00").replace(tzinfo=None)
@@ -29,18 +29,18 @@ start = datetime.utcnow() - timedelta(minutes=30)
 def get_data(start, stop):
     cursor = db.aggregate(pipeline=[
         {"$match": {"first": {"$lt": stop}, "last": {"$gt": start}}},
-        {"$match": {"data.ts": {"$lt": stop}, "data.ts": {"$gt": start}}},
+        {"$match": {"data.t": {"$lt": stop}, "data.t": {"$gt": start}}},
         {"$unwind": "$data"},
-        {"$group": {"_id": "$data.ts", "ts": {"$first": "$data.ts"}, "freq": {"$first": "$data.freq"},
-                    "calc_time": {"$first": "$data.calc_time"}}},
+        {"$group": {"_id": "$data.t", "t": {"$first": "$data.t"}, "f": {"$first": "$data.f"},
+                    "period_start": {"$first": "$data.period_start"}}},
     ])
     data = json_normalize(cursor).drop(["_id"], axis=1)
-    data = data.set_index("ts").sort_index()
+    data = data.set_index("t").sort_index()
     return data
 
 
 data = get_data(start=start, stop=stop)
-data["freq"].plot()
+data["f"].plot()
 plt.ylim([49.8, 50.2])
 plt.gca().axhline(y=50, color="grey", alpha=0.5)
 plt.show()
